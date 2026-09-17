29b6988d7c3beca5795371abd04d6754d4256a27
Wada <aidanharvey64@gmail.com>
2026-09-17 15:48:18 -0400

    adding mechanum tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -1,5 +1,15 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.tuning.autotune.Procedure;
+import com.pedropathing.tuning.autotune.Tuner;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.procedures.MecanumTuner;
+
 public class Tuning {
     // Tuners go here
+    @Tuner
+    public static Procedure mechanumTuner()
+    {
+        return new MecanumTuner();
+    }
 }
